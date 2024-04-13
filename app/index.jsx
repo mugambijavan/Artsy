090fa43f56fe33d499586ff860ed05a02c2856bc
Javoh <117713938+mugambijavan@users.noreply.github.com>
2024-04-14 02:07:15 +0300

    tabs

--- app/index.jsx
+++ app/index.jsx
@@ -11,7 +11,7 @@ export default function App() {
         <View className="flex-1 items-center justify-center bg-white">
         <Text className="text-3xl font-pblack">Artsy</Text>
         <StatusBar style="auto" />
-        <Link href="/home" style={{ color: 'cyan'}}>Go to home</Link>
+        <Link className="text-2xl " href="/home"  style={{ color: 'cyan'}}>Go to home</Link>
         </View>
     );
 }
