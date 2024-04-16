2f74d2daec74d3e05a9d98dd6ec1811d57ad3bba
Javoh <117713938+mugambijavan@users.noreply.github.com>
2024-04-16 12:12:26 +0300

    onboarding screen

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,4 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
 import { SplashScreen, Stack } from 'expo-router'
 import { useFonts } from 'expo-font'
 import { useEffect } from 'react'
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: " #008080",
+        primary: "#161622",
         secondary: {
           DEFAULT: "#FF9C01",
           100: "#FF9001",
